754f539642f436c624c4d45f8ac4b9108e0049af
Edgard Lopez <edgnando10@gmail.com>
2020-09-03 11:13:16 -0500

    docs agregado

---
+++ docs/precache-manifest.b51c785939d0cad26d6f394b451a0501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ae2f7e9c88f5e7dd51b1c6fe5840f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e73765c6697e8a8674ab",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e73765c6697e8a8674ab",
+    "url": "/static/js/main.2ad651e5.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
